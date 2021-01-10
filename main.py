66659b40f39b12606adf4f2eaa90e8d22566a8b2
Sophia <sophiaffmw@gmail.com>
2021-01-10 22:19:35 +0700

    fix bug

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 import torchvision
-import . import transforms
+from . import transforms
 import numpy as np
 import os
 import argparse
